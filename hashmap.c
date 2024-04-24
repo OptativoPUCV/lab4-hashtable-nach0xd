b7d9a3e648e88aadfe798b76c26a475513fee687
nach0xd <163194321+nach0xd@users.noreply.github.com>
2024-04-24 21:36:23 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -97,7 +97,6 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-  map->current = -1;
   for (long i = 0; i < map->capacity; i++){
     if (map->buckets[i] != NULL){
       map->current = i;
