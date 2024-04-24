a30f5b4d18e23093985b71f647bdd945f8eabbdd
nach0xd <163194321+nach0xd@users.noreply.github.com>
2024-04-24 21:45:30 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -61,7 +61,7 @@ void enlarge(HashMap * map) {
   enlarge_called = 1; //no borrar (testing purposes)
   Pair ** oldArr = map->buckets;
   map->capacity *= 2;
-  map->buckets = (Pair **) calloc(map->capacity * sizeof(Pair *));
+  map->buckets = (Pair **)calloc(map->capacity, sizeof(Pair *));
   map->size = 0;
   for (int i = 0; i < map->capacity / 2; i++){
     insertMap(map, oldArr[i]->key, oldArr[i]->value);
