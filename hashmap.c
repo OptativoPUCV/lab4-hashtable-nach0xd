d42cc31e0da7aeee8de2291ce8eedbcabb7d8c3e
nach0xd <163194321+nach0xd@users.noreply.github.com>
2024-04-24 20:57:13 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -77,7 +77,7 @@ void eraseMap(HashMap * map,  char * key) {
 	int pos = hash(key, map->capacity);
   while(map->buckets[pos] != NULL){
     if (is_equal(map->buckets[pos]->key, key)){
-      map->buckets[pos] = NULL;
+      map->buckets[pos]->key = NULL;
     }
     pos = (pos + 1) % map->capacity;
   }
