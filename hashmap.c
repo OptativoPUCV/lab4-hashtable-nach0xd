5882051db586c029a24267b03fc31ad3fed1ecd6
nach0xd <163194321+nach0xd@users.noreply.github.com>
2024-04-24 20:26:33 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -52,8 +52,12 @@ void enlarge(HashMap * map) {
 
 
 HashMap * createMap(long capacity) {
-
-    return NULL;
+  HashMap *map = (HashMap *)malloc(sizeof(HashMap));
+  map->buckets = (Pair **)calloc(capacity, sizeof(Pair *));
+	map->size = 0;
+  map->capacity = capacity;
+  map->current = -1;
+  return map;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
